b381e0ace6406559d83a692c818ea1d9996b20a0
Carlos Carrión <ccarrionro@gmail.com>
2024-10-22 17:19:57 +0200

    Actividad 1. Servlets

---
+++ src/main/java/ejemplos/servlet/curso/Main.java
@@ -0,0 +1,7 @@
+package ejemplos.servlet.curso;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! <p><a href=\"/myServlet2\">Vamos al otro Servlet</a></p>");
+    }
+}
